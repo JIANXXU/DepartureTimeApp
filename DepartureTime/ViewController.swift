40849a2145d53ee57af43b2cd695a7295ca25a16
Jian Xu <xujianoct@gmail.com>
2014-10-26 12:44:40 -0700

    Initial Commit

---
+++ DepartureTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DepartureTime
+//
+//  Created by Xu, Steven (Jian) on 10/26/14.
+//  Copyright (c) 2014 Xu, Steven (Jian). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
